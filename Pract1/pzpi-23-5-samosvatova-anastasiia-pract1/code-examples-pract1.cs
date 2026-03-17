b56a3447e8c28a4f929486ce77948a0b4c01d2f3
Samosvatova Anastasiia <anastasiia.samosvatova@nure.ua>
2026-03-17 18:01:54 +0200

    Add files via upload

---
+++ Pract1/pzpi-23-5-samosvatova-anastasiia-pract1/code-examples-pract1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public interface IShape
+{
+    void Draw();
+}
+
+public class Circle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Намальовано коло");
+    }
+}
+public class Rectangle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Намальовано прямокутник");
+    }
+}
+
+public static class ShapeFactory
+{
+    public static IShape CreateShape(string type)
+    {
+        if (type == "Circle")
+        {
+            return new Circle();
+        }
+
+        if (type == "Rectangle")
+        {
+            return new Rectangle();
+        }
+
+        throw new ArgumentException("Невідомий тип фігури");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IShape shape1 = ShapeFactory.CreateShape("Circle");
+        shape1.Draw();
+
+        IShape shape2 = ShapeFactory.CreateShape("Rectangle");
+        shape2.Draw();
+    }
+}
